a05288988af2772296eece69012020e54b292067
Cong123-h <criscong2005@gmail.com>
2025-10-21 17:45:39 +0700

    thêm api

---
+++ Project8/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace Project8.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Kích hoạt định tuyến theo attribute
+            config.MapHttpAttributeRoutes();
+
+            // Cấu hình route mặc định cho API
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new MediaTypeHeaderValue("text/html")
+            );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
