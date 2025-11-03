cf758fe2e964f229af583c7e92c441a024a26141
Cong123-h <criscong2005@gmail.com>
2025-11-03 20:24:18 +0700

    thêm chức năng

--- SHOP-FASHION/backend/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () =>{
-    mongoose.connection.on('connected',()=>{
-        console.log("Kết nối rồi hẹ hẹ hẹ");
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/Shop-Fashion`)
-}
-export default connectDB;
